9b492ecda442c49f36c836134270a88aca6b3a30
Raj Khandekar <prithviraj.k100@gmail.com>
2024-03-23 00:05:24 -0400

    Update tracker.py
    fix obsidian filename issue

--- tracker.py
+++ tracker.py
@@ -5,26 +5,28 @@ import subprocess
 from bs4 import BeautifulSoup
 from datetime import datetime
 import time
+import re
+
+def remove_special_characters(text):
+    pattern = r'[^a-zA-Z0-9\s]'
+    cleaned_text = re.sub(pattern, '', text)
+    return cleaned_text
 
 def get_linkedin_job_details(url):
     while True:
         try:
             response = requests.get(url, allow_redirects=True)        
             soup = BeautifulSoup(response.text, 'html.parser')
-            # Extract job title
             title = soup.find('h1').text.strip()
             
             print(title)
 
-            ## Extract Company Name
             company_name = soup.find('a', class_="topcard__org-name-link").text.strip()
             print(company_name)
 
-            ## Extract Location
             location = soup.find('span', class_="topcard__flavor--bullet").text.strip()
             print(location)
             
-            # Extract job description
             article_elem = soup.find('div', class_="description__text")
             description=article_elem.get_text(strip=True) if article_elem else "Job Description not found"
             print(description)
@@ -41,18 +43,18 @@ def get_linkedin_job_details(url):
             return None, None, None, None
 
 def write_to_file(title, company_name, location, description):
-    # Get current year and month
     current_year = datetime.now().strftime("%Y")
     current_month = datetime.now().strftime("%B")
 
     folder_path = os.path.join(current_year, current_month, "Summary")
     os.makedirs(folder_path, exist_ok=True)
+    
+    clean_title = remove_special_characters(title)
+    clean_company_name = remove_special_characters(company_name)
 
-    # Construct the full file path
-    filename = f"{company_name}-{title}"
+    filename = f"{clean_company_name}-{clean_title}"
     file_path = os.path.join(folder_path, f"{filename}.md")
 
-    # Write job details to Markdown file
     with open(file_path, 'w') as file:
         file.write(f"# {company_name} - {title}\n\n")
         file.write(location+"\n")
@@ -60,35 +62,27 @@ def write_to_file(title, company_name, location, description):
 
     print(f"Job details written to {file_path}")
 
-    # Update the main summary table
     update_summary_table(filename)
 
 def update_summary_table(job_file_name):
-    # Get current year and month
     current_year = datetime.now().strftime("%Y")
     current_month = datetime.now().strftime("%B")
 
-    # Construct the full path for the main summary markdown file
     summary_file_path = os.path.join(current_year, current_month, "index.md")
 
-    # Check if the summary file exists
     if not os.path.exists(summary_file_path):
-        # If it doesn't exist, create the summary file with the header
         with open(summary_file_path, 'w') as summary_file:
             summary_file.write("| Summary | Date |\n")
             summary_file.write("| ------- | ---- |\n")
 
-    # Get the current date
     current_date = datetime.now().strftime("%Y-%m-%d")
 
-    # Append a new row to the summary table
     with open(summary_file_path, 'a') as summary_file:
         summary_file.write(f"| [[{job_file_name}]] | {current_date} |\n")
 
     print(f"Summary table updated in {summary_file_path}")
 
 def handler(url):
-    # Check if the URL belongs to Workopolis
     if "workopolis" in url:
         print("Detected workopolis url, sending curl request...")
         curl_command = ["curl", "-A", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36", "-H", "Accept-Language: en-US,en;q=0.5", "-L", url]
